350ab77268261f27fc9317482f22f3abcac81b5f
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-12-28 21:47:09 +0900

    [2024-12-28]: FragmentManager에 UI 프래그먼트 추가

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -12,5 +12,12 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null){
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
+
     }
 }
