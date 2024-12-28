51b86c151858673c78bb7633f0922311ccff8672
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2024-12-28 22:11:28 +0900

    [2024-12-28]: 주석 추가

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -17,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         if(currentFragment == null){
             val fragment = CrimeFragment()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+            // 프래그먼트 매니저에 접근해서 트랜잭션을 생성하고 생성된 트랜잭션으로 프래그먼트를 추가
+            // 프래그먼트 트랜잭션은 프래그먼트를 추가 삭제 변경 첨부 등을 하는 데 사용 됨.
+            // 즉 프래그먼트를 다룰 땐 트랜잭션이 꼭 필요
         }
 
     }
