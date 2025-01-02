cc0cfa05107bd02c8c4583f9ebff772e1115c574
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-01-02 16:40:53 +0900

    [2024-12-29]: 카메라 기능 추가

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -14,4 +14,7 @@ data class Crime(
     var isSolved: Boolean = false,
     var suspect: String = ""
 //    var requiresPolice: Boolean = false
-)
+) {
+    val photoFileName
+        get() = "IMG_$id.jpg"
+}
